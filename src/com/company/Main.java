1a4323d414262425f93ad71f33b4dd8d98efaf84
arkad <mojib.khabireh@gmail.com>
2022-04-06 10:28:09 +0430

    make socket and send to server some data

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        String host="localhost";
+        Integer port=9999;
+        Socket socket = new Socket(host,port);
+        OutputStream outputStream = OutputStream.nullOutputStream(socket.getOutputStream());
+        PrintWriter printWriter = new PrintWriter(outputStream);
+        printWriter.write("hhhhhh");
+    }
+}
---
+++ src/com/company/client/Client.java
@@ -0,0 +1,24 @@
+package com.company.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    String host="localhost";
+    Integer port=9999;
+    Socket socket = new Socket(host,port);
+
+
+    public Client() throws IOException {
+    }
+    public void client() throws IOException {
+        OutputStream outputStream = OutputStream.nullOutputStream(socket.getOutputStream());
+        PrintWriter printWriter = new PrintWriter(outputStream);
+        printWriter.write("hhhhhh");
+
+    }
+
+}
+
